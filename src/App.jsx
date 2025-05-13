249f6a3eb6ad0a1e18c4146864f820ced486003c
Ishan Singh <74155919+Ishan5529@users.noreply.github.com>
2025-05-13 11:49:58 +0530

    Setup NeetoUI and installed all dependencies

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { SideBar } from "components/common";
+import { Kanban } from "components/Kanban";
+import { News } from "components/News";
+import { Pomodoro } from "components/Pomodoro";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { routes } from "routes";
+
+const App = () => (
+  <div className="flex h-screen">
+    <SideBar />
+    <div className="flex-1 overflow-hidden p-4">
+      <Switch>
+        <Route exact component={Kanban} path={routes.productivity.index} />
+        <Route exact component={Pomodoro} path={routes.productivity.pomodoro} />
+        <Route exact component={News} path={routes.productivity.news} />
+        <Redirect exact from={routes.root} to={routes.productivity.index} />
+      </Switch>
+    </div>
+  </div>
+);
+
+export default App;
