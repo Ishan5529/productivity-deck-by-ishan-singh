b7ed77ce41f3d6c3134adddf1c674453f7320d5f
Ishan5529 <ishan2004.official@gmail.com>
2025-05-13 09:46:15 +0000

    Added Filter Functionality

--- src/components/News/constant.js
+++ src/components/News/constant.js
@@ -17,3 +17,26 @@ export const isEmptyOrUndefined = either(isEmpty, isNil);
 export const DEFAULT_PAGE_SIZE = 2;
 
 export const DEFAULT_PAGE_INDEX = 1;
+
+export const newsCategoryOptions = [
+  { label: "Business", value: "business" },
+  { label: "Entertainment", value: "entertainment" },
+  { label: "General", value: "general" },
+  { label: "Health", value: "health" },
+  { label: "Science", value: "science" },
+  { label: "Sports", value: "sports" },
+  { label: "Technology", value: "technology" },
+];
+
+export const newsOptions = [
+  { label: "BBC News", value: "bbc-news" },
+  { label: "The Verge", value: "the-verge" },
+  { label: "Business Insider", value: "business-insider" },
+  { label: "Time", value: "time" },
+  { label: "The Next Web", value: "the-next-web" },
+  { label: "ABC News", value: "abc-news" },
+  { label: "Engadget", value: "engadget" },
+  { label: "Entertainment Weekly", value: "entertainment-weekly" },
+  { label: "ESPN", value: "espn" },
+  { label: "Financial Post", value: "financial-post" },
+];
